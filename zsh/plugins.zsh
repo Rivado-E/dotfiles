3005345bcfdab248aad8a55a3c2eedee79197bd5
rivado-e <redah@umd.edu>
2025-03-21 09:27:05 -0400

    saved for later

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -21,19 +21,8 @@ source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-# Plugin history-search-multi-word loaded with investigating.
-# zinit load zdharma-continuum/history-search-multi-word
-
-# Two regular plugins loaded without investigating.
-# zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma-continuum/fast-syntax-highlighting
-# zinit light zsh-users/zsh-autosuggestions
-
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+zinit light zsh-users/zsh-autosuggestions
 
 zinit load zsh-users/zsh-history-substring-search
 zinit ice wait atload'_history_substring_search_config'
@@ -43,8 +32,5 @@ HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="fg=none,bg=none"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="fg=none,bg=none"
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=true
 
-
-zinit ice depth=1
-zinit light jeffreytse/zsh-vi-mode
-
-# source zsh-history-substring-search.zsh
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
